a022ce845af5c8d349d0a52b0522f523ea6195a4
zhangjiechina001 <295737015@qq.com>
2023-10-23 21:05:00 +0800

    完善ocr相关代码

--- ocr/example.py
+++ ocr/example.py
@@ -1,11 +1,41 @@
+import numpy as np
 import pytesseract
-from PIL import Image
+from pytesseract import Output
+import cv2
 
-# Open an image using the Python Imaging Library (PIL)
-image = Image.open('img1.png')
-pytesseract.pytesseract.tesseract_cmd='C:/Program Files (x86)/Tesseract-OCR/tesseract.exe'
-# Use pytesseract to extract text from the image
-text = pytesseract.image_to_string(image)
+pytesseract.pytesseract.tesseract_cmd="C:/Program Files (x86)/Tesseract-OCR/tesseract.exe"
+try:
+    from PIL import Image
+    from PIL import ImageDraw
+    from PIL import ImageFont
+except ImportError:
+    import Image
 
-# Print the extracted text
-print(text)
+img = cv2.imread('img2.png')
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+
+width_list = []
+for c in cnts:
+	_, _, w, _ = cv2.boundingRect(c)
+	width_list.append(w)
+wm = np.median(width_list)
+
+tess_text = pytesseract.image_to_data(img, output_type=Output.DICT, lang='chi_sim')
+for i in range(len(tess_text['text'])):
+	word_len = len(tess_text['text'][i])
+	if word_len > 1:
+		world_w = int(wm * word_len)
+		(x, y, w, h) = (tess_text['left'][i], tess_text['top'][i], tess_text['width'][i], tess_text['height'][i])
+		cv2.rectangle(img, (x, y), (x + world_w, y + h), (255, 0, 0), 1)
+		im = Image.fromarray(img)
+		draw = ImageDraw.Draw(im)
+		font = ImageFont.truetype(font="simsun.ttc", size=18, encoding="utf-8")
+		draw.text((x, y - 20), tess_text['text'][i], (255, 0, 0), font=font)
+		img = cv2.cvtColor(np.array(im), cv2.COLOR_RGB2BGR)
+
+cv2.imshow("TextBoundingBoxes", img)
+cv2.waitKey(0)
