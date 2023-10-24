4dee4c60e90a61e23d4265783503a789966d90fd
zhangjiechina001 <295737015@qq.com>
2023-10-25 07:29:40 +0800

    加入字符方框注释

--- ocr/example2.py
+++ ocr/example2.py
@@ -8,21 +8,23 @@ image = Image.open('fullimg.bmp')
 
 # 使用Pytesseract进行字符识别
 text = pytesseract.image_to_string(image)
-
+print(text)
 # 将图像转换为OpenCV格式
 image_cv = cv2.cvtColor(cv2.imread('fullimg.bmp'), cv2.COLOR_BGR2RGB)
 
+custom_config =r'--psm 6 -c tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 # 获取识别的字符边界框
-detection_boxes = pytesseract.image_to_boxes(image)
+detection_boxes = pytesseract.image_to_boxes(image,output_type=pytesseract.Output.STRING,config=custom_config)
 
 img_h,img_w,_=image_cv.shape
 # 遍历每个字符的边界框并绘制方框
 for box in detection_boxes.splitlines():
     box = box.split()
-    x, y, w, h = int(box[1]), int(box[2]), int(box[3]), int(box[4])
-    cv2.rectangle(image_cv, (x, y), (w, h), (0, 255, 0), 2)
+    #矩形的两个对角点
+    x1, y1, x2, y2 = int(box[1]), int(box[2]), int(box[3]), int(box[4])
+    cv2.rectangle(image_cv, (x1, img_h-y1), (x2, img_h-y2), (0, 255, 0), 2)
     # cv2.putText(image_cv, x,(x, h), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-    # cv2.putText(image_cv, box[0], (x, y - 2), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
+    cv2.putText(image_cv, box[0], (x1, img_h-y1), cv2.FONT_HERSHEY_SIMPLEX,  0.5, (0, 255, 255), 2)
 
 # 显示带有方框标注的图像
 cv2.imshow('Image with Boxes', image_cv)
