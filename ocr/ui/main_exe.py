55420bdb988eea42e7deee6bf1d6055896615bfd
zhangjiechina001 <295737015@qq.com>
2023-10-26 07:44:02 +0800

    优化ui逻辑

--- ocr/ui/main_exe.py
+++ ocr/ui/main_exe.py
@@ -1,12 +1,25 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
+from main_ui import Ui_MainWindow
 
-import main_ui
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.setWindowTitle('字符识别')
+        self.ui.btnOpenFile.clicked.connect(self.open_file)
+
+    def open_file(self):
+        options = QFileDialog.Options()
+        file_name, _ = QFileDialog.getOpenFileName(self, "Open File", "",
+                                                   "All Files (*);;Text Files (*.txt);;Python Files (*.py)",
+                                                   options=options)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    MainWindow = QMainWindow()
-    ui = main_ui.Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
+    ui = MainWindow()
+    ui.show()
     sys.exit(app.exec_())
